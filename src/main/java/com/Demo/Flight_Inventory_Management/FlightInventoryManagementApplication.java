946b9ffc0a8f72f28dbea70f080c06cf311d9bcf
El mahjoubi Ayoub <schwert.ayoub@gmail.com>
2024-10-24 15:55:25 -0700

    start spring boot project

---
+++ src/main/java/com/Demo/Flight_Inventory_Management/FlightInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Demo.Flight_Inventory_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Demo/Flight_Inventory_Management/FlightInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Demo.Flight_Inventory_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
