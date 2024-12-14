fad3a02c6c577bd3210dba5e1f82d900159c4c65
AyoubElmahjoubi <168941231+AyoubElmahjoubi@users.noreply.github.com>
2024-12-14 11:03:48 -0800

    fixing email test server

--- src/main/java/com/Demo/Flight_Inventory_Management/email/EmailService.java
+++ src/main/java/com/Demo/Flight_Inventory_Management/email/EmailService.java
@@ -4,6 +4,7 @@ package com.Demo.Flight_Inventory_Management.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Slf4j
 @RequiredArgsConstructor
 public class EmailService {
+
 
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
